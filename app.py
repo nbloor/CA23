5803cbedd5746fe785e0aa8440494992403d400f
Nicholas Bloor <161645384+nbloor@users.noreply.github.com>
2024-10-01 12:06:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import streamlit as st
+
+st.title("My Streamlit App")
+
+col1, col2 = st.column(2)
+
+with col1:
+  st.header("Column 1")
+  st.write("Hello")
+
+with col2:
+  st.header("Column 2")
+  st.write("World")
+
+with st.expander("Expand for more information"):
+  st.write("Here you can put in some really explanatory things.")
